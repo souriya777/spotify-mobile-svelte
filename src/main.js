f6bc402c4c25e81dabab081e2f8578650145d12d
Souriya PHONGSAVANH <benjamin.franklin59777@gmail.com>
2023-07-06 15:18:34 +0200

    add path alias "@"

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import './css/main.scss'
-import App from './App.svelte'
+import '@/css/main.scss'
+import App from '@/App.svelte'
+import { toto } from '@/js/toto'
+
+toto()
 
 const app = new App({
   target: document.getElementById('app'),
